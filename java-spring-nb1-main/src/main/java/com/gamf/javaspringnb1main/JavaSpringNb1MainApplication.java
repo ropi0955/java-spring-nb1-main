d84e897ebf78c964e479ded9f186d745e1fc84e7
ropi0955 <szemanyik.gergely@gmail.com>
2023-11-21 20:17:26 +0100

    project creation in intelij

---
+++ java-spring-nb1-main/src/main/java/com/gamf/javaspringnb1main/JavaSpringNb1MainApplication.java
@@ -0,0 +1,13 @@
+package com.gamf.javaspringnb1main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringNb1MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringNb1MainApplication.class, args);
+    }
+
+}
---
+++ java-spring-nb1-main/src/test/java/com/gamf/javaspringnb1main/JavaSpringNb1MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamf.javaspringnb1main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringNb1MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
